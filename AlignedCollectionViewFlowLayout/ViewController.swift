9b4fa55c46e50388eaf36d89140f0209c97211c6
Kuntulu Ankita <kuntuluankita@gmail.com>
2023-10-06 11:07:25 +0530

    Initial Commit

---
+++ AlignedCollectionViewFlowLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlignedCollectionViewFlowLayout
+//
+//  Created by K Praveen Kumar on 06/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
